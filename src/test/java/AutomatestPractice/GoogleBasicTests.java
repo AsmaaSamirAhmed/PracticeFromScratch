d2c436baf64717810088fb331a43e6995e7ea119
asmaa <asmaasamir229@gmail.com>
2026-08-05 17:36:24 +0300

    Comment out GoogleBasicTests class and its methods

--- src/test/java/AutomatestPractice/GoogleBasicTests.java
+++ src/test/java/AutomatestPractice/GoogleBasicTests.java
@@ -5,20 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GoogleBasicTests extends TestCase {
-    WebDriver driver;
-
-    @Test
-    public void checkGooglePageUrl() {
-        driver.navigate().to("https://www.google.com/");
-        String currentUrl = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrl, "https://www.google.com/");
-    }
-    @Test
-    public void checkGooglePageTitle() {
-        driver.navigate().to("https://www.google.com/");
-        String currentTitle = driver.getTitle();
-        Assert.assertEquals(currentTitle, "Google");
-    }
-
-}
+//public class GoogleBasicTests extends TestCase {
+//    WebDriver driver;
+//
+//    @Test
+//    public void checkGooglePageUrl() {
+//        driver.navigate().to("https://www.google.com/");
+//        String currentUrl = driver.getCurrentUrl();
+//        Assert.assertEquals(currentUrl, "https://www.google.com/");
+//    }
+//    @Test
+//    public void checkGooglePageTitle() {
+//        driver.navigate().to("https://www.google.com/");
+//        String currentTitle = driver.getTitle();
+//        Assert.assertEquals(currentTitle, "Google");
+//    }
+//
+//}
