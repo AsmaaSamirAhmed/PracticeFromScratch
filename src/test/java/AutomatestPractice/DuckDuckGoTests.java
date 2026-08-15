274e3e9c237eb2ae128d8a3cd3faf8e97491f918
asmaa <asmaasamir229@gmail.com>
2026-08-15 18:34:14 +0300

    Add DuckDuckGo URL to config and update tests to use it

--- src/test/java/AutomatestPractice/DuckDuckGoTests.java
+++ src/test/java/AutomatestPractice/DuckDuckGoTests.java
@@ -2,19 +2,20 @@ package AutomatestPractice;
 
 import Pages.DuckDuckGo;
 import Templates.TestCase;
+import Utils.ConfigReader;
 import org.testng.annotations.Test;
 
 @Test
 public class DuckDuckGoTests extends TestCase {
     public void checkDuckDuckGoLogo() {
         DuckDuckGo duckDuckGo = new DuckDuckGo(bot);
-        duckDuckGo.navigateTo("https://duckduckgo.com/")
+        duckDuckGo.navigateTo(ConfigReader.getProperty("DuckDuckGoURL"))
                 .assertLogo();
     }
 
     public void checkResultContainsCucmberDocs() {
         DuckDuckGo duckDuckGo = new DuckDuckGo(bot);
-        duckDuckGo.navigateTo("https://duckduckgo.com/")
+        duckDuckGo.navigateTo(ConfigReader.getProperty("DuckDuckGoURL"))
                 .search("Cucumber IO")
                 .checkSecondLinkContainsCucumber("https://cucumber.io/docs/");
     }
